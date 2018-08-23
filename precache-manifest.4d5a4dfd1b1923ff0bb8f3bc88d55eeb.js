2d09e53dfc5ce4c327086074e2e31f4e2d416910
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-23 13:43:05 +0000

    Deploy Kyr/CV to github.com/Kyr/CV.git:gh-pages

---
+++ precache-manifest.4d5a4dfd1b1923ff0bb8f3bc88d55eeb.js
@@ -0,0 +1,24 @@
+self.__precacheManifest = [
+  {
+    "url": "main.34013c3846a9ead734fb.js"
+  },
+  {
+    "url": "main.34013c3846a9ead734fb.css"
+  },
+  {
+    "revision": "d9d397562e125af885daa3032ac95633",
+    "url": "index.html"
+  },
+  {
+    "revision": "87fcf21ac521ea6db2680415004f2d3e",
+    "url": "assets/photo/w400.jpg"
+  },
+  {
+    "revision": "5b8f058d00253203e228b14d5147fa3e",
+    "url": "assets/photo/w300.jpg"
+  },
+  {
+    "revision": "c126da6055e50337ba0cf34067c5def1",
+    "url": "assets/photo/photo.jpg"
+  }
+];
