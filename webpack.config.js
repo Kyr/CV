7bad28614971c31cffcd5b888de7157a5b01d195
Kyrylo Fedorov <kyrylofedorov@gmail.com>
2018-09-03 09:42:23 +0300

    Update:
    - somme changes in style for mobile;
    - tiny changes in scripts;
    - small improve in webpack config for better DX;

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@ const HtmlWebpackPlugin    = require('html-webpack-plugin');
 const CopyWebpackPlugin    = require('copy-webpack-plugin');
 const CleanWebpackPlugin   = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+const WorkboxPlugin        = require('workbox-webpack-plugin');
 
-const getContent           = require('./scripts/content');
+const getContent   = require('./scripts/content');
+const fallbackArgv = {mode: 'development'}; // to allow webStorm to parse config
 
-module.exports = (env, argv) => {
+module.exports = (env, argv = fallbackArgv) => {
   const prodMode = argv.mode === 'production';
 
   return {
@@ -15,12 +16,12 @@ module.exports = (env, argv) => {
       './src/index.js',
       // style: './src/style.scss',
     ],
-/*
-    entry:     {
-      app:   './src/index.js',
-      style: './src/style.scss',
-    },
-*/
+    /*
+        entry:     {
+          app:   './src/index.js',
+          style: './src/style.scss',
+        },
+    */
     output:    {
       filename: '[name].[hash].js',
     },
@@ -37,27 +38,29 @@ module.exports = (env, argv) => {
           content: getContent(resolve(__dirname, 'README.md')),
         },
       }),
-      new CopyWebpackPlugin([{
-        from:   'assets/photo/',
-        to:     'assets/photo/',
-        toType: 'dir',
-        /*
-              // TODO
-              transform (content, path) {
-                return optimize(content)
-              // or
-                return responsive(content, srcset)
-              // or composition of above
-              }
-        */
-      }], {/*options*/}),
+      new CopyWebpackPlugin([
+        {
+          from:   'assets/photo/',
+          to:     'assets/photo/',
+          toType: 'dir',
+          /*
+                // TODO
+                transform (content, path) {
+                  return optimize(content)
+                // or
+                  return responsive(content, srcset)
+                // or composition of above
+                }
+          */
+        },
+      ], {/*options*/}),
       new MiniCssExtractPlugin({
         filename:      prodMode ? '[name].[hash].css' : '[name].css',
         chunkFilename: prodMode ? '[id].[hash].css' : '[id].css',
       }),
       new WorkboxPlugin.GenerateSW({
         clientsClaim: true,
-        skipWaiting: true,
+        skipWaiting:  true,
       }),
     ],
     module:    {
